3b963a65884293aec15abf86f124866e0c7d0e75
CloudRex <cloudrex@outlook.com>
2018-12-28 17:52:54 -0500

    Add option for output

--- src/predefied-ops/git.ts
+++ src/predefied-ops/git.ts
@@ -2,91 +2,91 @@ import ScriptOperations from "./scripts";
 
 export default abstract class GitOperations {
     public static pull(): Promise<boolean> {
-        return ScriptOperations.execute("git", "pull");
+        return ScriptOperations.execute("git", ["pull"]);
     }
 
     public static pullSync(): boolean {
-        return ScriptOperations.executeSync("git", "pull");
+        return ScriptOperations.executeSync("git", ["pull"]);
     }
 
     public static push(): Promise<boolean> {
-        return ScriptOperations.execute("git", "push");
+        return ScriptOperations.execute("git", ["push"]);
     }
 
     public static pushSync(): boolean {
-        return ScriptOperations.executeSync("git", "push");
+        return ScriptOperations.executeSync("git", ["push"]);
     }
 
     public static merge(branch: string): Promise<boolean> {
-        return ScriptOperations.execute("git", "merge", branch);
+        return ScriptOperations.execute("git", ["merge", branch]);
     }
 
     public static mergeSync(branch: string): boolean {
-        return ScriptOperations.executeSync("git", "merge", branch);
+        return ScriptOperations.executeSync("git", ["merge", branch]);
     }
 
     public static branch(name: string): Promise<boolean> {
-        return ScriptOperations.execute("git", "checkout", name);
+        return ScriptOperations.execute("git", ["checkout", name]);
     }
 
     public static branchSync(name: string): boolean {
-        return ScriptOperations.executeSync("git", "checkout", name);
+        return ScriptOperations.executeSync("git", ["checkout", name]);
     }
 
     public add(items: string = "*"): Promise<boolean> {
-        return ScriptOperations.execute("git", "add", items);
+        return ScriptOperations.execute("git", ["add", items]);
     }
 
     public addSync(items: string = "*"): boolean {
-        return ScriptOperations.executeSync("git", "add", items);
+        return ScriptOperations.executeSync("git", ["add", items]);
     }
 
     public static commit(message: string): Promise<boolean> {
-        return ScriptOperations.execute("git", "commit", "-m", message);
+        return ScriptOperations.execute("git", ["commit", "-m", message]);
     }
 
     public static commitSync(message: string): boolean {
-        return ScriptOperations.executeSync("git", "commit", "-m", message);
+        return ScriptOperations.executeSync("git", ["commit", "-m", message]);
     }
 
     public static commitAll(message: string): Promise<boolean> {
-        return ScriptOperations.execute("git", "commit", "-am", message);
+        return ScriptOperations.execute("git", ["commit", "-am", message]);
     }
 
     public static commitAllSync(message: string): boolean {
-        return ScriptOperations.executeSync("git", "commit", "-am", message);
+        return ScriptOperations.executeSync("git", ["commit", "-am", message]);
     }
 
     public static revert(commit: string = "HEAD"): Promise<boolean> {
-        return ScriptOperations.execute("git", "revert", commit);
+        return ScriptOperations.execute("git", ["revert", commit]);
     }
 
     public static revertSync(commit: string = "HEAD"): boolean {
-        return ScriptOperations.executeSync("git", "revert", commit);
+        return ScriptOperations.executeSync("git", ["revert", commit]);
     }
 
     public static createBranch(name: string): Promise<boolean> {
-        return ScriptOperations.execute("git", "checkout", "-b", name);
+        return ScriptOperations.execute("git", ["checkout", "-b", name]);
     }
 
     public static createBranchSync(name: string): boolean {
-        return ScriptOperations.executeSync("git", "checkout", "-b", name);
+        return ScriptOperations.executeSync("git", ["checkout", "-b", name]);
     }
 
     public static deleteBranch(name: string): Promise<boolean> {
-        return ScriptOperations.execute("git", "branch", "-D", name);
+        return ScriptOperations.execute("git", ["branch", "-D", name]);
     }
 
     public static deleteBranchSync(name: string): boolean {
-        return ScriptOperations.executeSync("git", "branch", "-D", name);
+        return ScriptOperations.executeSync("git", ["branch", "-D", name]);
     }
 
     public static setUpstream(remoteBranch: string, remote: string = "origin"): Promise<boolean> {
-        return ScriptOperations.execute("git", "branch", `--set-upstream-to=${remote}/${remoteBranch}`);
+        return ScriptOperations.execute("git", ["branch", `--set-upstream-to=${remote}/${remoteBranch}`]);
     }
 
     public static setUpstreamSync(remoteBranch: string, remote: string = "origin"): boolean {
-        return ScriptOperations.executeSync("git", "branch", `--set-upstream-to=${remote}/${remoteBranch}`);
+        return ScriptOperations.executeSync("git", ["branch", `--set-upstream-to=${remote}/${remoteBranch}`]);
     }
 
     public static clone(url: string, path?: string): Promise<boolean> {
@@ -96,7 +96,7 @@ export default abstract class GitOperations {
             params.push(path);
         }
 
-        return ScriptOperations.execute("git", ...params);
+        return ScriptOperations.execute("git", params);
     }
 
     public static cloneSync(url: string, path?: string): boolean {
@@ -106,6 +106,6 @@ export default abstract class GitOperations {
             params.push(path);
         }
 
-        return ScriptOperations.executeSync("git", ...params);
+        return ScriptOperations.executeSync("git", params);
     }
 }
--- src/predefied-ops/scripts.ts
+++ src/predefied-ops/scripts.ts
@@ -1,9 +1,13 @@
 import {exec, execSync} from "child_process";
 
 export default abstract class ScriptOperations {
-    public static execute(base: string, ...args: string[]): Promise<boolean> {
+    public static execute(base: string, params: string[], output: boolean = false): Promise<boolean> {
         return new Promise((resolve) => {
-            exec(`${base} ${args.join(" ")}`.trim(), (error: Error | null) => {
+            exec(`${base} ${params.join(" ")}`.trim(), (error: Error | null, out: string) => {
+                if (output) {
+                    console.log(out);
+                }
+
                 if (error !== null) {
                     resolve(false);
 
@@ -15,9 +19,13 @@ export default abstract class ScriptOperations {
         });
     }
 
-    public static executeSync(base: string, ...args: string[]): boolean {
+    public static executeSync(base: string, params: string[], output: boolean = false): boolean {
         try {
-            execSync(`${base} ${args.join(" ")}`.trim());
+            const out: string = execSync(`${base} ${params.join(" ")}`.trim()).toString();
+
+            if (output) {
+                console.log(out);
+            }
 
             return true;
         }
@@ -26,39 +34,39 @@ export default abstract class ScriptOperations {
         }
     }
 
-    public static npm(name: string): Promise<boolean> {
-        return ScriptOperations.execute("npm", "run-script", name);
+    public static npm(name: string, output?: boolean): Promise<boolean> {
+        return ScriptOperations.execute("npm", ["run-script", name], output);
     }
 
-    public static npmSync(name: string): boolean {
-        return ScriptOperations.executeSync("npm", "run-script", name);
+    public static npmSync(name: string, output?: boolean): boolean {
+        return ScriptOperations.executeSync("npm", ["run-script", name], output);
     }
 
-    public static npmStart(): Promise<boolean> {
-        return ScriptOperations.execute("npm", "start");
+    public static npmStart(output?: boolean): Promise<boolean> {
+        return ScriptOperations.execute("npm", ["start"], output);
     }
 
-    public static npmTest(): Promise<boolean> {
-        return ScriptOperations.execute("npm", "test");
+    public static npmTest(output?: boolean): Promise<boolean> {
+        return ScriptOperations.execute("npm", ["test"], output);
     }
 
-    public static npmTestSync(): boolean {
-        return ScriptOperations.executeSync("npm", "test");
+    public static npmTestSync(output?: boolean): boolean {
+        return ScriptOperations.executeSync("npm", ["test"], output);
     }
 
-    public static npmBuild(): Promise<boolean> {
-        return ScriptOperations.npm("build");
+    public static npmBuild(output?: boolean): Promise<boolean> {
+        return ScriptOperations.npm("build", output);
     }
 
-    public static npmBuildSync(): boolean {
-        return ScriptOperations.npmSync("build");
+    public static npmBuildSync(output?: boolean): boolean {
+        return ScriptOperations.npmSync("build", output);
     }
 
-    public static npmInstall(): Promise<boolean> {
-        return ScriptOperations.execute("npm", "install");
+    public static npmInstall(output?: boolean): Promise<boolean> {
+        return ScriptOperations.execute("npm", ["install"], output);
     }
 
-    public static npmInstallSync(): boolean {
-        return ScriptOperations.executeSync("npm", "install");
+    public static npmInstallSync(output?: boolean): boolean {
+        return ScriptOperations.executeSync("npm", ["install"], output);
     }
 }
