81264998b5cef01189a86e1beb58f8f9c90efcfd
CloudRex <cloudrex@outlook.com>
2019-02-02 23:39:02 -0500

    Add default tasks

--- src/tasks/runner.ts
+++ src/tasks/runner.ts
@@ -6,6 +6,15 @@ import ScriptOps from "../predefied-ops/script";
 const args: string[] = process.argv.slice(2);
 const taskName: string | undefined = args[0];
 
+// Register default tasks.
+Tasks.set("build", () => {
+    ScriptOps.npmBuildSync(true);
+});
+
+Tasks.set("test", () => {
+    ScriptOps.npmTestSync(true);
+});
+
 // Run npm start.
 if (taskName === undefined) {
     ScriptOps.npmStartSync(true);
